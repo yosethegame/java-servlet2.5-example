85d67db28a82f5b94e10e1b686e4eef81390f794
Eric Mignot <eric.mignot@gmail.com>
2013-11-09 13:15:22 -0500

    passes ping level of yose

---
+++ src/test/java/yose/PingChallengeTest.java
@@ -0,0 +1,40 @@
+package yose;
+
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import org.ericminio.htmlunit.FeatureDescription;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PingChallengeTest extends FeatureDescription {
+
+    private WebClient client = new WebClient();
+    private WebResponse response;
+
+    @Before
+    public void openPage() throws IOException {
+        Page page = client.getPage( "http://localhost:8080/ping" );
+        response = page.getWebResponse();
+    }
+
+    @Test
+    public void pingAnswers() throws IOException {
+        assertThat( response.getStatusCode(), equalTo( 200 ));
+    }
+
+    @Test
+    public void pingAnswersJson() throws IOException {
+        assertThat( response.getContentType(), equalTo( "application/json" ));
+    }
+
+    @Test
+    public void pingAnswersTheValueExpectedByTheGame() {
+        assertThat( response.getContentAsString(), equalTo( "{\"alive\":true}" ));
+    }
+}
--- src/test/org/yose/PingChallengeTest.java
+++
@@ -1,4 +0,0 @@
-package org.yose;
-
-public class PingChallengeTest {
-}
